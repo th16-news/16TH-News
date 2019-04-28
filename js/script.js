d19e168eca5de1b398d1ab71fe0f354b7f995efd
dang khoi <dangkhoi.tinhoc1@gmail.com>
2019-04-28 23:45:03 +0700

    Add login, search, logout, profile to navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-// carousel
+// navbar
+// show search-bar
+
+function showSearchBar() {
+    var searchbar = document.getElementById("search-bar");
+    var toggle = document.getElementsByClassName("search-toggle")[0];
+
+    if (searchbar.style.visibility === "hidden" || searchbar.style.visibility == "") {
+        searchbar.style.visibility = "visible";
+        toggle.innerHTML = '<i class="fas fa-times"></i>';
+    } else {
+        searchbar.style.visibility = "hidden";
+        toggle.innerHTML = '<i class="fas fa-search"></i>';
+    }
+}
